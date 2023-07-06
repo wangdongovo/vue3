01eb372eab09746f2e624cc2d56ff485546daebd
wd <wd@dddeMacBook-Pro.local>
2023-07-06 18:15:57 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  build: {
+    outDir: 'html',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
